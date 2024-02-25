7cd1cf45ef36a914388fb8b4107e56bb15262bc6
chigoKawa <kecsdbeats@gmail.com>
2024-02-25 14:31:06 +0100

    deletede sav-back
    deletede sav-back

--- sav-front/app/(regular)/profile/page.tsx
+++ sav-front/app/(regular)/profile/page.tsx
@@ -1,17 +1,6 @@
 import React from 'react'
-import {
-  readItems,
-  authentication,
-  refresh,
-  createDirectus,
-  rest,
-  readCollections,
-  readCollection,
-  readMe,
-  withOptions,
-  withToken
-} from '@directus/sdk'
-import directus from '@/utils/directus.js'
+
+
 import { cookies } from 'next/headers'
 
 const ProfilePage = async () => {
@@ -19,18 +8,7 @@ const ProfilePage = async () => {
   const accessToken: any = cookieStore.get('access_token')?.value || ''
 
   let currentUser: any = ''
-  try {
-    currentUser = await directus.request(
-      withToken(
-        accessToken,
-        readMe({
-          fields: ['*']
-        })
-      )
-    )
-  } catch (error) {
-    console.error('za currentUser error', error)
-  }
+
 
   return (
     <div>
